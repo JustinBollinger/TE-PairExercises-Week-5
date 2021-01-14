d22d36fb54717ee65a84f1ff073d8b6ecb9d89c1
Justin Bollinger <jbollingerinterpreter@gmail.com>
2021-01-14 15:05:44 -0500

    Updated INSERT-UPDATE-DELETE Pair Exercises -JB

--- module-2/04_INSERT_UPDATE_and_DELETE/pair-exercises/constraints-and-transactions-exercises-pair.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/pair-exercises/constraints-and-transactions-exercises-pair.sql
@@ -59,11 +59,21 @@ AND district = 'Kansas';
 
 -- 6. Return the US captial to Washington.
 
+UPDATE country
+SET capital = 3813
+WHERE capital = 4080;
 
+SELECT *
+FROM country
+WHERE code = 'USA';
 
 
 -- 7. Delete Smallville, Kansas from the city table. (Did it succeed? Why?)
+-- Succeeded because there were no dependencies from other tables since we UPDATED the capital to Washington.
 
+DELETE FROM city
+WHERE name = 'Smallville'
+AND district = 'Kansas';
 
 
 
@@ -72,14 +82,29 @@ AND district = 'Kansas';
 -- (exclusive). 
 -- (590 rows affected)
 
+-- !!! COME BACK !!! --
+
+UPDATE countrylanguage
+SET cl.isofficial = NOT cl.isofficial
+FROM countylanguage AS cl
+LEFT JOIN country AS c
+        ON c.code = cl.countrycode
+WHERE indepyear BETWEEN 1801 AND 1971;
 
 
 
 -- 9. Convert population so it is expressed in 1,000s for all cities. (Round to
 -- the nearest integer value greater than 0.)
 -- (4079 rows affected)
+-- ** Confused about "expressed in 1,000s **
+-- Database successfully updated, but not sure if population values are what was desired.
 
+UPDATE city
+SET population = ROUND(population / 1000, 0);
+WHERE population != 0;
 
+SELECT *
+FROM city;
 
 
 -- 10. Assuming a country's surfacearea is expressed in square miles, convert it to 
@@ -87,5 +112,22 @@ AND district = 'Kansas';
 -- population.
 -- (7 rows affected)
 
-
-
+UPDATE country
+SET surfacearea = (surfacearea * 1609.34)
+WHERE code IN('MCO'
+        , 'FRA'
+        , 'BEL'
+        , 'CAN'
+        , 'MYT'
+        , 'PYF'
+        , 'NCL');
+        
+SELECT *
+FROM country
+WHERE code IN('MCO'
+        , 'FRA'
+        , 'BEL'
+        , 'CAN'
+        , 'MYT'
+        , 'PYF'
+        , 'NCL');
